be827fc0ee9fbebdf5fc8a7e3dc14c58f32dd1ea
Baderfall <delete-863@yandex.by>
2017-09-20 13:00:09 +0300

    Middleware for store.dispatch added

--- app/configureStore.js
+++ app/configureStore.js
@@ -2,42 +2,45 @@ import { createStore } from 'redux';
 
 import todoApp from './reducers';
 
-const addLoggingToDispatch = (store) => {
-  const originalDispatch = store.dispatch;
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.forEach((middleware) => {
+    store.dispatch = middleware(store)(store.dispatch);
+  });
+};
+
+const logger = store => next => {
   if(!console.group) {
-    return originalDispatch;
+    return next;
   }
 
   return (action) => {
     console.group(action.type);
     console.log('%c prev state:', 'color: gray', store.getState());
     console.log('%c action:', 'color: blue', action);
-    const returnValue = originalDispatch(action);
+    const returnValue = next(action);
     console.log('%c new state:', 'color: green', store.getState());
     console.groupEnd(action.type);
     return returnValue;
   };
 };
 
-const addPromiseToDispatch = (store) => {
-  const originalDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(originalDispatch);
-    }
-    return originalDispatch;
-  };
+const promise = () => next => action => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next;
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  const middlewares = [];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
+  middlewares.push(promise);
 
-  store.dispatch = addPromiseToDispatch(store);
-
+  wrapDispatchWithMiddlewares(store, middlewares);
   return store;
 };
 
